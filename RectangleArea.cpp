62ae223da86d068e4627ddc4915667d3b3b6c8fc
Remas-khalid <Remaskhalid800@gmail.com>
2025-11-15 03:21:49 +0300

    Create RectangleArea.cpp
    Calculates the area of a rectangle based on user input for length and width.

---
+++ RectangleArea.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+float length , width , area ;
+
+
+cout<<"enter length and width to calc the area : "<<endl;
+  
+cout<<"length : "<<endl;
+cin>>length ; 
+  
+cout<<"width: "<<endl;
+cin>>width ; 
+  
+area=length*width;
+cout<<"area is :"<<area ;
+  
+  return 0; 
+}
